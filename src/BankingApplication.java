7542a48466606b1ef342cfffc4822313462a91b6
amanbagda45 <bagdaaman94@gmail.com>
2025-09-19 20:33:17 -0500

    Initial commit

---
+++ src/BankingApplication.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class BankingApplication {
+    public static void main(String[] args){
+        BankAccount obj = new BankAccount("Aman","BA0016");
+        obj.showMenu();
+    }
+}
+
+class BankAccount{
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    BankAccount(String cname,String cid){
+        customerName = cname;
+        customerId = cid;
+    }
+
+    void deposit(int amount){
+        if (amount != 0){
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount){
+        if (amount != 0){
+            balance = balance -amount;
+            previousTransaction = -amount;
+        }
+    }
+    void getPreviousTransaction(){
+        if(previousTransaction > 0){
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdraw: " +Math.abs(previousTransaction));
+        }else {
+            System.out.println("No transaction occured");
+        }
+    }
+
+    void showMenu(){
+        char option='\0';
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome " +customerName);
+        System.out.println("Your id is "+customerId);
+//        System.out.println("\n");
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous transaction");
+        System.out.println("D. Exit");
+
+        do {
+            System.out.println("===============================================");
+            System.out.println("Enter an option");
+            option = scanner.next().charAt(0);
+//            System.out.println("\n");
+            switch (option){
+                case 'A':
+                    System.out.println("-------------------------");
+                    System.out.println("Balance ="+balance);
+                    System.out.println("-------------------------");
+//                    System.out.println("\n");
+                    break;
+
+                case 'B':
+                    System.out.println("-------------------------");
+                    System.out.println("Enter an amount to deposit:");
+                    System.out.println("-------------------------");
+                    int amount = scanner.nextInt();
+                    deposit(amount);
+//                    System.out.println("\n");
+                    break;
+
+                case 'C':
+                    System.out.println("-------------------------");
+                    System.out.println("Enter an amount to withdraw");
+                    System.out.println("-------------------------");
+                    int amount2 =scanner.nextInt();
+                    withdraw(amount2);
+//                    System.out.println("\n");
+                    break;
+
+                case 'D':
+                    System.out.println("-------------------------");
+                    getPreviousTransaction();
+                    System.out.println("-------------------------");
+//                    System.out.println("\n");
+                    break;
+
+                case 'E':
+                    System.out.println("-------------------------");
+                    break;
+
+                default:
+                    System.out.println("Invalid option!. Please enter again");
+                    break;
+            }
+        }while (option != 'E');
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,115 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//int n=5;
+//for (int i=0; i<n;i++){
+//    for (int j=1;j<n-i;j++){
+//        System.out.print(" ");
+//    }
+//    for (int k=0;k<i*2+1;k++){
+//        System.out.print("*");
+//    }
+//    System.out.println();
+//}
+
+
+//        int a=5;
+//        for (int i=0;i<a;i++){
+//            for (int j=0;j<=i;j++){
+//                System.out.print(" * ");
+//            }
+//            System.out.println(" ");
+//        }
+
+
+//        int a=5;
+//        for (int i=1;i<=a;i++){
+//            for (int j=1;j<=a;j++){
+//                if (i==1 || i ==a || j == 1 || j==a){
+//                System.out.print("* ");}
+//                else {
+//                    System.out.print("  ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
+//        int a=4;
+//        for (int i=1;i<=a;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        int a=5;
+//                for (int i=1;i<=a;i++){
+//            for (int j=1;j<=a-i+1;j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        int a=5;
+//        int num=1;
+//        for(int i=1;i<=a;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print(num++ + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        int n=5;
+//        int mid=n/2+1;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n;j++){
+//                if(j==mid || i+j==mid +1||j==mid+i-1){
+//                    System.out.print("* ");
+//                }else
+//                    System.out.print(" ");
+//
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+//        int n =5;
+//        int m=n/2+1;
+//        for(int i=1; i<=n; i++) {
+//            for (int j=1; j<=n; j++) {
+//                if(j==m||i+j==m+1||j==m+i-1)
+//                    System.out.print("* ");
+//                else
+//                    System.out.print("  ");
+//            }
+//            System.out.println();
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
